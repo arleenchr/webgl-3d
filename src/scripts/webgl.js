afaa3038e09daacf4ea4ee9cecd99a79b4c16369
arleenchr <13521059@mahasiswa.itb.ac.id>
2024-04-30 14:06:22 +0700

    Feat: Init WebGL Shader

---
+++ src/scripts/webgl.js
@@ -0,0 +1,48 @@
+var canvas = document.querySelector("#gl-canvas")
+var gl = canvas.getContext("webgl")
+
+if (!gl){
+    console.error("Unable to initialize WebGL.")
+} else {
+    console.log("Initialize successful.")
+}
+
+function createShader(gl, type, source){
+    var shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader)
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        console.error("Failed to compile shader: " + gl.getShaderInfoLog(shader));
+        gl.deleteShader(shader);
+        return;
+    }
+    return shader;
+}
+
+function createProgram(gl, vertexShader, fragmentShader){
+    const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vertexShaderText);
+    const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fragmentShaderText);
+    const program = gl.createProgram();
+
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+
+    if (!gl.getProgramParameter(program, gl.LINK_STATUS)){
+        console.error("Failed to create program: " + gl.getProgramInfoLog(program));
+        gl.deleteProgram(program);
+        return;
+    }
+
+    gl.validateProgram(program);
+    if (!gl.getProgramParameter(program, gl.VALIDATE_STATUS)) {
+        console.error("Failed to validate program!", gl.getProgramInfoLog(program));
+        gl.deleteProgram(program);
+        return;
+    }
+
+    gl.deleteShader(vertexShader);
+    gl.deleteShader(fragmentShader);
+
+    return program;
+}
