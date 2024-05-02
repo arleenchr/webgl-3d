bf230b5d5c558170bd77faefe97de9613830c03a
billc27 <110593711+billc27@users.noreply.github.com>
2024-05-03 04:45:23 +0700

    feat: add shaders

--- src/scripts/webgl.js
+++ src/scripts/webgl.js
@@ -1,3 +1,45 @@
+// Shaders
+const vertex_shader = `
+attribute vec3 aPosition;
+attribute vec3 aColor;
+attribute vec3 aNormal;
+
+varying vec4 fragColor;
+varying vec4 vNormal;
+varying float colorFactor;
+
+uniform float fudgeFactor;
+uniform mat4 uTransformationMatrix;
+uniform mat4 uViewMatrix;
+uniform mat4 uProjectionMatrix;
+uniform mat4 uNormalMatrix;
+
+void main(void) {
+    vec4 transformedPos = uTransformationMatrix * vec4(aPosition, 1.0);
+    vec4 transformedNormal = uTransformationMatrix * vec4(aNormal, 1.0);
+    vec4 projectedPos = uProjectionMatrix * transformedPos;
+    if (fudgeFactor < 0.01)
+        gl_Position = projectedPos;
+    else {
+        float zDivider = 1.0 + projectedPos.z * fudgeFactor;
+        gl_Position = vec4(projectedPos.xy / zDivider, projectedPos.zw);
+    }
+  
+    vNormal = uNormalMatrix * vec4(aNormal, 0.0);
+    fragColor = vec4(aColor, 1.0);    
+    colorFactor = min(max((1.0 - transformedPos.z) / 2.0, 0.0), 1.0);
+}
+`;
+
+const fragment_shader = `
+precision mediump float;
+varying vec4 fragColor;
+
+void main(void) {
+    gl_FragColor = fragColor;
+}
+`;
+
 var canvas = document.querySelector("#gl-canvas")
 var gl = canvas.getContext("webgl")
 
@@ -7,7 +49,7 @@ if (!gl){
     console.log("Initialize successful.")
 }
 
-function createShader(gl, type, source){
+function createShader(gl, type, source) {
     var shader = gl.createShader(type);
     gl.shaderSource(shader, source);
     gl.compileShader(shader)
@@ -19,9 +61,10 @@ function createShader(gl, type, source){
     return shader;
 }
 
-function createProgram(gl, vertexShaderText, fragmentShaderText){
+function createProgram(gl, vertexShaderText, fragmentShaderText) {
     const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderText);
     const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderText);
+
     const program = gl.createProgram();
 
     gl.attachShader(program, vertexShader);
