d2bd0184d61c912976544de78b0415b991800e66
billc27 <110593711+billc27@users.noreply.github.com>
2024-05-18 16:05:39 +0700

    refactor: create model function to utils

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -105,3 +105,64 @@ const generateCuboid = (height, width, depth, origin) => {
         colors: colors,
     }
 }
+
+const createModelSides = (model, arr) => {
+    let faces = [];
+    let colors = [];
+    let vertices = [];
+
+    let arrLen = arr.length;
+    let verLen = model.vertices.length;
+
+    // Create inward and outward faces
+    for (let i = 0; i < arr.length - 2; i++) {
+        faces.push(
+            [verLen + 1, verLen + 2 + i, verLen + 3 + i],
+            [verLen + 1, verLen + 3 + i, verLen + 2 + i]
+        );
+    }
+  
+    // Create random colors
+    for (let i = 0; i < arrLen; i++) {
+        colors.push([Math.random(), Math.random(), Math.random()]);
+    }
+  
+    // Add arr to vertices
+    vertices.push(...arr);
+
+    return { vertices, faces, colors };
+}
+
+const create3dModel = (model, vert) => {
+    let len = vert.length / 4;
+
+    for (let i = 0; i < len; i++) {
+        let x = vert.slice(i * 4, (i + 1) * 4);
+        let y = createModelSides(model, x);
+    
+        model.vertices.push(...y.vertices);
+        model.faces.push(...y.faces);
+        model.colors.push(...y.colors);
+    }
+  
+    let faceLen = model.faces.length;
+    let normals = Array(faceLen).fill([]);
+
+    for (let i = 0; i < faceLen; i++) {
+        let selectedFaces = model.faces[i];
+        selectedFaces = selectedFaces.map((x) => x - 1);
+    
+        let x = model.vertices[selectedFaces[0]];
+        let y = model.vertices[selectedFaces[1]];
+        let z = model.vertices[selectedFaces[2]];
+    
+        let selectedArr = [x, y, z];
+        let normal = vectors.calculateNormal(selectedArr);
+
+        for (let i = 0; i < 3; i++) {
+            let selectedIdx = selectedFaces[i];
+            normals[selectedIdx] = normal;
+        }
+    }
+    model.normals = normals;
+}
