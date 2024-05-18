dd514585a1f4d7265e46e0144b09b5336facf783
Chris Albert <christiangultom5@gmail.com>
2024-05-18 17:06:10 +0700

    feat: animate articulated object

--- src/scripts/cube.js
+++ src/scripts/cube.js
@@ -1,10 +1,36 @@
 const cube = new Object();
 cube.name = 'cube';
 cube.model = generateCuboid(0.2, 1.2, 0.1, [0, 0, 0]);
+cube.animation = [
+    {
+        translate: [-1, 0, 0],
+        rotate: [0 , 0, 0],
+        scale: [1, 1, 1],
+    },
+    {
+        translate: [1, 0, 0],
+        rotate: [0, 0, 0],
+        scale: [1, 1, 1],
+    }
+];
+addFramesToObject(cube);
 
 const cubeLeg = new Object();
 cubeLeg.name = 'cubeLeg';
 cubeLeg.model = generateCuboid(1.2, 0.2, 0.1, [0, -0.5, 0]);
+cubeLeg.animation = [
+    {
+        translate: [0, 0, 0],
+        rotate: [0 , 0, 0],
+        scale: [1, 1, 1],
+    },
+    {
+        translate: [0, 0, 0],
+        rotate: [0, 0, Math.PI * 2],
+        scale: [1, 1, 1],
+    }
+];
+addFramesToObject(cubeLeg);
 
 cubeLeg.setParent(cube);
 
