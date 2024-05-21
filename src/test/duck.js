8da008a91e524f0b140e3898b7d1dc1af3220233
billc27 <110593711+billc27@users.noreply.github.com>
2024-05-21 15:11:14 +0700

    feat: add duck articulated model animation

--- src/test/duck.js
+++ src/test/duck.js
@@ -1,53 +1,282 @@
 const duck = new Object();
 duck.name = 'duck';
 
+let still = {
+    translate: [0, 0, 0],
+    rotate: [0, 0, 0],
+    scale: [1, 1, 1],
+};
+
 // Height, Width, Depth
 duck.model = generateCuboid(0.5, 0.5, 0.55, [0, 0, 0]);
 
+duck.animation = [
+    {
+        translate: [0, 0, 0],
+        rotate: [0, Math.PI/2, 0],
+        scale: [1, 1, 1],
+    },
+    {
+        translate: [0, 0, 0],
+        rotate: [0, Math.PI/2, 0],
+        scale: [1, 1, 1],
+    },
+    {
+        translate: [0, 0, 0],
+        rotate: [0, Math.PI/2, 0],
+        scale: [1, 1, 1],
+    },
+    still,
+    still,
+    still,
+    {
+        translate: [0, 0, 0],
+        rotate: [0, -Math.PI/2, 0],
+        scale: [1, 1, 1],
+    },
+    {
+        translate: [0, 0, 0],
+        rotate: [0, -Math.PI/2, 0],
+        scale: [1, 1, 1],
+    },
+    {
+        translate: [0, 0, 0],
+        rotate: [0, -Math.PI/2, 0],
+        scale: [1, 1, 1],
+    },
+];
+addFramesToObject(duck);
+
+const duckNeck = new Object();
+duckNeck.name = 'neck';
+duckNeck.model = generateCuboid(0.3, 0.15, 0.15, [0, 0.3, -0.2]);
+duckNeck.setParent(duck);
+
+duckNeck.animation = [
+    still,
+    {
+        translate: [0, 0, 0.1],
+        rotate: [0, 0, 0],
+        scale: [1, 1, 1],
+    },
+    still,
+    {
+        translate: [0, 0, 0.075],
+        rotate: [0, 0, 0],
+        scale: [1, 1, 1],
+    },
+    still,
+    {
+        translate: [0, 0, 0.075],
+        rotate: [0, 0, 0],
+        scale: [1, 1, 1],
+    },
+    still,
+    {
+        translate: [0, 0, 0.075],
+        rotate: [0, 0, 0],
+        scale: [1, 1, 1],
+    },
+    still,
+];
+addFramesToObject(duckNeck);
+
+
 const duckHead = new Object();
-duckHead.name = 'head'; // 0.35
+duckHead.name = 'head';
 duckHead.model = generateCuboid(0.35, 0.3, 0.3, [0, 0.6, -0.25]);
-duckHead.setParent(duck);
+duckHead.setParent(duckNeck);
+
+duckHead.animation = [
+    still,
+    still,
+    still,
+    still,
+    still,
+    still,
+    still,
+    still,
+    still,
+];
+addFramesToObject(duckHead);
 
 const duckBeak = new Object();
 duckBeak.name = 'beak';
 duckBeak.model = generateCuboid(0.1, 0.2, 0.2, [0, 0.525, -0.4]);
 duckBeak.setParent(duckHead);
 
+duckBeak.animation = [
+    still,
+    still,
+    still,
+    still,
+    still,
+    still,
+    still,
+    still,
+    still,
+];
+addFramesToObject(duckBeak);
+
 const duckLowerBody = new Object();
 duckLowerBody.name = 'lower-body';
 duckLowerBody.model = generateCuboid(0.3, 0.35, 0.5, [0, -0.3, 0]);
 duckLowerBody.setParent(duck);
 
-const duckNeck = new Object();
-duckNeck.name = 'neck';
-duckNeck.model = generateCuboid(0.3, 0.15, 0.15, [0, 0.3, -0.2]);
-duckNeck.setParent(duckLowerBody);
+duckLowerBody.animation = [
+    still,
+    still,
+    still,
+    still,
+    still,
+    still,
+    still,
+    still,
+    still,
+];
+addFramesToObject(duckLowerBody);
 
 const duckTail = new Object();
 duckTail.name = 'tail';
 duckTail.model = generateCuboid(0.15, 0.1, 0.05, [0, 0, 0.3]);
 duckTail.setParent(duckLowerBody);
 
+duckTail.animation = [
+    still,
+    {
+        translate: [0, -0.1, 0],
+        rotate: [0, 0, 0],
+        scale: [1, 1, 1],
+    },
+    still,
+    {
+        translate: [0.1, 0, 0],
+        rotate: [0, 0, 0],
+        scale: [1, 1, 1],
+    },
+    {
+        translate: [-0.1, 0, 0],
+        rotate: [0, 0, 0],
+        scale: [1, 1, 1],
+    },
+    {
+        translate: [0, 0, 0],
+        rotate: [0, 0, 0],
+        scale: [1, 1, 1],
+    },
+    still,
+    {
+        translate: [0, -0.1, 0],
+        rotate: [0, 0, 0],
+        scale: [1, 1, 1],
+    },
+    still,
+];
+addFramesToObject(duckTail);
+
 const duckLeg1 = new Object();
 duckLeg1.name = 'leg-1';
 duckLeg1.model = generateCuboid(0.5, 0.1, 0.1, [0.1, -0.5, 0]);
 duckLeg1.setParent(duck);
+
+duckLeg1.animation = [
+    still,
+    {
+        translate: [0, 0, 0],
+        rotate: [-Math.PI/6, 0, 0],
+        scale: [1, 1, 1],
+    },
+    still,
+    {
+        translate: [0, 0, 0],
+        rotate: [Math.PI/6, 0, 0],
+        scale: [1, 1, 1],
+    },
+    still,
+    {
+        translate: [0, 0, 0],
+        rotate: [-Math.PI/6, 0, 0],
+        scale: [1, 1, 1],
+    },
+    still,
+    {
+        translate: [0, 0, 0],
+        rotate: [Math.PI/6, 0, 0],
+        scale: [1, 1, 1],
+    },
+    still,
+];
+addFramesToObject(duckLeg1);
 
 const duckLeg2 = new Object();
 duckLeg2.name = 'leg-2';
 duckLeg2.model = generateCuboid(0.5, 0.1, 0.1, [-0.1, -0.5, 0]);
 duckLeg2.setParent(duck);
 
+duckLeg2.animation = [
+    still,
+    {
+        translate: [0, 0, 0],
+        rotate: [Math.PI/6, 0, 0],
+        scale: [1, 1, 1],
+    },
+    still,
+    {
+        translate: [0, 0, 0],
+        rotate: [-Math.PI/6, 0, 0],
+        scale: [1, 1, 1],
+    },
+    still,
+    {
+        translate: [0, 0, 0],
+        rotate: [Math.PI/6, 0, 0],
+        scale: [1, 1, 1],
+    },
+    still,
+    {
+        translate: [0, 0, 0],
+        rotate: [-Math.PI/6, 0, 0],
+        scale: [1, 1, 1],
+    },
+    still,
+];
+addFramesToObject(duckLeg2);
+
 const duckFeet1 = new Object();
 duckFeet1.name = 'feet-1';
 duckFeet1.model = generateCuboid(0.1, 0.15, 0.2, [0.1, -0.75, -0.02]);
 duckFeet1.setParent(duckLeg1);
 
+duckFeet1.animation = [
+    still,
+    still,
+    still,
+    still,
+    still,
+    still,
+    still,
+    still,
+    still,
+];
+addFramesToObject(duckFeet1);
+
 const duckFeet2 = new Object();
 duckFeet2.name = 'feet-2';
 duckFeet2.model = generateCuboid(0.1, 0.15, 0.2, [-0.1, -0.75, -0.02]);
 duckFeet2.setParent(duckLeg2);
+
+duckFeet2.animation = [
+    still,
+    still,
+    still,
+    still,
+    still,
+    still,
+    still,
+    still,
+    still,
+];
+addFramesToObject(duckFeet2);
 
 var duckObject = [
     duck,
